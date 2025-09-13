3c69369b9090b6c92ca6d676bf68e1a97e4e29ed
Nicholas Bartlett <nicholas_bartlett_30.09@outlook.de>
2025-09-13 10:50:07 +0200

    smoothe enemy movement system, new tile system related to that

--- filereader.py
+++ filereader.py
@@ -5,20 +5,11 @@ from tile import TILE
 def read_map():
     map_array = []
     temp_array = []
-    with open("maps/map1.csv","r") as file:
+    with open("maps/map2.csv","r") as file:
         reader = csv.reader(file)
         for i, line in enumerate(reader):
             for j, tile in enumerate(line):
-                match tile:
-                    case "0":
-                        temp_array.append(TILE(j, i, int(tile), False))
-                    case "1":
-                        temp_array.append(TILE(j, i, int(tile), True))
-                    case "2":
-                        temp_array.append(TILE(j, i, int(tile), True))
-                    case "3":
-                        temp_array.append(TILE(j, i, int(tile), True))
-
+                temp_array.append(TILE(j, i, int(tile), False))
             map_array.append(temp_array)
             temp_array = []
     return map_array
--- tile.py
+++ tile.py
@@ -1,7 +1,15 @@
+import pygame
+
 class TILE:
-    def __init__(self, x, y, field_type, can_collide_with, tower = None):
+    def __init__(self, x, y, field_type, can_collide_with, tower = None, rect =  None,):
         self.x = x
         self.y = y
         self.field_type = field_type
         self.can_collide_with = can_collide_with
         self.tower = tower
+        if rect is None:
+            self.rect = pygame.Rect(0,0,0,0)
+        self.on_tile = 1
+
+    def __str__(self):
+        return "{}".format(self.field_type)
